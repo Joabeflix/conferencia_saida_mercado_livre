05a5734870abcb235a974f849a0720069783d87d
Joabe Alves <joabealvesyt@gmail.com>
2025-08-26 11:26:39 -0300

    deleted:    teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-import winsound
-
-winsound.PlaySound('audio.wav', winsound.SND_FILENAME)
-winsound.PlaySound('audio.wav', winsound.SND_FILENAME)
-winsound.PlaySound('audio.wav', winsound.SND_FILENAME)
-
-
-
-
